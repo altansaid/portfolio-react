7d250967de1b93cdab9f3daccb5a6436cf2502c3
Said <alta0073@algonquinlive.com>
2025-06-09 12:54:49 -0400

    syntax error solved

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -141,11 +141,7 @@ export const HeroSection = () => {
 						</p>
 					</div>
 					<div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
-<<<<<<< HEAD
 						<a href='/resume_said_altan_june9.pdf' download>
-=======
-						<a href='/Said_Altan_Resume.pdf' download>
->>>>>>> b2f6ac246b28447455ce3712055dd9f323f88c4b
 							<button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
 								<span className='font-semibold'>Download Resume</span>
 								<ArrowDown className='size-4' />
